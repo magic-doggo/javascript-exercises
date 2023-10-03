d2fabfe3db96a6e6fa313760af6f595c464ff0d1
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-03 15:06:05 +0000

    Pass first 4 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  return string.repeat(num);
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
