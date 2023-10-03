4e16d4253c6a40815ae93a812d3a7983591f0e3c
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-03 16:28:27 +0000

    Final answer and recommended answer

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let length = string.length;
+  const array = [];
+  for (let i = 0; i < length; i++) {
+    array[length - i - 1] = string[i];
+  }
+  
+  let stringRev = array.join("")
+  return(stringRev);
 };
+
+//Recommended answer below. Mine works too though!
+// const reverseString = function (string) {
+//   return string.split("").reverse().join("");
+// };
 
 // Do not edit below this line
 module.exports = reverseString;
