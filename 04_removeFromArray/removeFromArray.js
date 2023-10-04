12658b7be13f5cf91bbfc3a214f474c4766a50d8
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-04 12:49:46 +0000

    Final response before comparing solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, value1, value2, value3, value4) {
+  let i = 0;
+  while (i < array.length) {
+    if (array[i] === value1 || array[i] === value2 || array[i] === value3 || array[i] === value4) {
+      array.splice(i, 1);
+    } else {
+      ++i;
+    }
+  }
+  return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
