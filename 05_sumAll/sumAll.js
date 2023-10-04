ba5d62de3f29ae6e37c049e1c8235bf54bc5c56a
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-04 16:15:47 +0000

    Solution with for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,42 @@
-const sumAll = function(firstNumber, lastNumber) {
-  let sum;
-  if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number' || firstNumber < 0 || lastNumber < 0 ){
-    return 'ERROR';
-  }
-  else if (firstNumber > lastNumber) {
-    sum = (firstNumber + lastNumber) * (firstNumber - lastNumber + 1) / 2;
-    return sum;
-  }
-  else {
-    sum = (firstNumber + lastNumber) * (lastNumber - firstNumber + 1) / 2 ;
-    return sum;
-  }
-};
+// const sumAll = function(firstNumber, lastNumber) {
+//   let sum;
+//   if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number' || firstNumber < 0 || lastNumber < 0 ){
+//     return 'ERROR';
+//   }
+//   else if (firstNumber > lastNumber) {
+//     sum = (firstNumber + lastNumber) * (firstNumber - lastNumber + 1) / 2;
+//     return sum;
+//   }
+//   else {
+//     sum = (firstNumber + lastNumber) * (lastNumber - firstNumber + 1) / 2 ;
+//     return sum;
+//   }
+// };
 
 //sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
-//pitagora : (1+10)x10/2
+
+const sumAll = function(firstNumber, lastNumber) {
+  let biggerNumber;
+  let smallerNumber;
+  if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number' || firstNumber < 0 || lastNumber < 0 ){
+  return 'ERROR';
+  }
+  else if (firstNumber > lastNumber) {
+    biggerNumber = firstNumber;
+    smallerNumber = lastNumber;
+  }
+  else {
+    biggerNumber = lastNumber;
+    smallerNumber = firstNumber
+  }
+  let sum = 0;
+  for ( let i = smallerNumber; i <= biggerNumber; i++){
+    sum += i;
+  }
+  return sum;
+}
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
