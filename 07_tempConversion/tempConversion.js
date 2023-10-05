6a5b642a96d97beeb81c6869085c1499f5191eb0
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-05 05:03:33 +0000

    Solve all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,18 @@
 const convertToCelsius = function(fToC) {
-  celsius = (fToC- 32) * 5/9
-  return celsius;
+  celsius = (fToC- 32) * 5/9;
+  roundedCelsius = Math.round(celsius * 10) / 10
+  return roundedCelsius;
 };
-
+//suggested solution:
+//const convertToCelsius = function (fahrenheit) {
+//   return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+// };
 
 
 const convertToFahrenheit = function(cToF) {
   fahrenheit = (cToF * 9) / 5 + 32;
-  return fahrenheit;
+  roundedFahrenheit = Math.round(fahrenheit * 10) / 10
+  return roundedFahrenheit;
 };
 //x °C ≘ (x × 9 / 5 + 32) °F
 //x °F ≘ (x − 32) × 5 / 9 °C
