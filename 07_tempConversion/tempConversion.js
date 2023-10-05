b7b6caa35281a9720a4f54f6b65a64e86a4e030d
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-05 04:52:34 +0000

    Solve first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fToC) {
+  celsius = (fToC- 32) * 5/9
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(cToF) {
+  fahrenheit = (cToF * 9) / 5 + 32;
+  return fahrenheit;
 };
+//x °C ≘ (x × 9 / 5 + 32) °F
+//x °F ≘ (x − 32) × 5 / 9 °C
+
 
 // Do not edit below this line
 module.exports = {
