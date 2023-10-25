21036d1d8bae5ca0d272b028403f84b4c4dd6ded
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-25 17:00:58 +0000

    Add solution fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(indexNumber) {
+  let fibonacciNumber;
+  let fnminus1 = 1;
+  let fnminus2 = 1;
+  if (parseInt(indexNumber) == 1 || (indexNumber) == 2) {
+    return 1;
+  }
+  if ((parseInt(indexNumber)) < 0){
+    return "OOPS"
+  }
+  for (let i = 0; i < indexNumber -2 ; i++){
+      fibonacciNumber = fnminus1 + fnminus2;
+      fnminus2 = fnminus1;
+      fnminus1 = fibonacciNumber;
+  }
+  return(fibonacciNumber)
 };
 
 // Do not edit below this line
