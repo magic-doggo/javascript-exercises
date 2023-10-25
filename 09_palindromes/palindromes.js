3fa7ad272045a6d00bba2eceb288458f30d5373a
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-25 15:30:49 +0000

    Add palindomes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // let noPunctuation = string.replace(/!.\s,/g, "")
+  let noPunctuation = string.match(/[^\W]+/g).join('').toLowerCase()
+  let reverse = noPunctuation.split("").reverse().join("");
+  if (noPunctuation == reverse){
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
