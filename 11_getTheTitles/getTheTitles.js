d0ffcb9dee7ca74b6529254ea8055bbca0e811bd
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-26 06:55:09 +0000

    Add solution getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let booksArray = []
+  for (let i = 0; i < books.length; i++){
+    booksArray.push(books[i]["title"])  
+  }
+  console.log(booksArray)
+  return(booksArray)
 };
+
+// const getTheTitles = function (array) {
+//   return array.map(function(book) {
+//     return book.title
+//   });
+// }; recommended solution modified from arrow function
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
