5297af6a66bd5c34bb87174c664b01032223048f
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-29 06:57:35 +0000

    Add for my for loop solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldestGuyAge = 0;
+  let personAge;
+  let oldestGuy;
+  for (let i = 0; i < people.length; i++ ){
+    if (people[i].yearOfDeath == undefined) {
+      people[i].yearOfDeath = currentDate.getFullYear();
+    }
+    personAge = people[i].yearOfDeath - people[i].yearOfBirth
+    if (personAge > oldestGuyAge) {
+        oldestGuyAge = personAge
+        oldestGuy = people[i]
+    }
+  }
+  return oldestGuy
 };
+
+const currentDate = new Date;
 
 // Do not edit below this line
 module.exports = findTheOldest;
